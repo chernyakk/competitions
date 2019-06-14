fade63260d1471d16e166a9d059ff16d425e0cc2
Alexander Kirshin <alexander.kirshin.dev@gmail.com>
2019-06-14 17:34:28 +0000

    Develop

--- resources/views/app/contest/list-cards.blade.php
+++ resources/views/app/contest/list-cards.blade.php
@@ -3,23 +3,93 @@
 @section('content')
     <div class="list-group">
         <div class="back">
-            <a href="{{ route('listContest') }}"><button class="btn btn-outline-primary">Вернуться назад</button></a>
-                <a href="/cards/contest/{{$contestId}}/allcards/" class="float-right"><button class="btn btn-danger">Список карточек</button></a>
-                <a href="/cards/contest/{{$contestId}}/print-cards/" class="float-right"><button class="btn btn-success">Печать карточек</button></a>
+            <a href="{{ route('listContest') }}"><button class="btn btn-outline-primary">К списку сорвенований</button></a>
+            <a href="/contest/{{$contestId}}" class="float-right">
+                <button class="btn btn-danger">Результаты соревнования</button>
+            </a>
+            <a href="/cards/contest/{{$contestId}}/allcards/" class="float-right">
+                <button class="btn btn-success">Печать карточек</button>
+            </a>
         </div>
         <br>
-        @foreach($sportsmen as $key => $item)
-        <div class="list-group-item list-group-item-action flex-column align-items-start">
-            <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1 text-dark">№ {{ $item->sector }} {{ $item->sportsman }}</h5>
-                <small class="text-primary">{{ \App\Models\Contest::where('id', $item->contest_id)->first()->name }}</small>
+
+        @foreach($sportsmenInComp as $only)
+            <div>
+                <h2>№ {{ $only->sector }} {{ $only->sportsman }}</h2>
+
+                <br>
+                <table class="table table-bordered table-striped myclass">
+                    <thead>
+                    <tr>
+                        <th rowspan="2" scope="col"></th>
+                        <th rowspan="2" scope="col">Сектор</th>
+                        <th rowspan="2" scope="col">Соперник</th>
+                        <th colspan="2" scope="col">Поимки</th>
+                        <th rowspan="2" scope="col">Результат (баллов):</th>
+                    </tr>
+                    <tr>
+                        <th>Личные</th>
+                        <th>Соперника</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <div style="display: none">
+                        {{$data = \Illuminate\Support\Facades\DB::table('sportsmen')
+                            ->join('results', 'sportsmen.id', '=', 'results.sportsman_id')
+                            ->where('results.contest_id', '=', $contestId)
+                            ->where('results.sector', '=', $only->sector)
+                            ->select('sportsmen.sportsman', 'results.id', 'results.sportsman_id', 'results.contest_id', 'results.tour_id',
+                                'results.point', 'results.haul', 'results.place', 'results.sector', 'results.sector_type')
+                            ->get()}}
+                    </div>
+                    @foreach($data as $item)
+                        <tr>
+                            <th scope="row">Тур {{ $item->tour_id }}</th>
+                            <td>{{ $item->place }}</td>
+                            <td>
+                                № {{ $n = \Illuminate\Support\Facades\DB::table('sportsmen')
+                 ->join('results', 'sportsmen.id', '=', 'results.sportsman_id')
+                 ->where('results.contest_id', '=', $item->contest_id)
+                 ->where('results.tour_id', '=', $item->tour_id)
+                 ->where('results.place', '=', $item->place)
+                 ->where('results.sportsman_id', '<>', $item->sportsman_id)
+                 ->select('results.sector')
+                 ->value('sector')
+                 }}
+
+                                {{ $s = \Illuminate\Support\Facades\DB::table('sportsmen')
+                                 ->join('results', 'sportsmen.id', '=', 'results.sportsman_id')
+                                 ->where('results.contest_id', '=', $item->contest_id)
+                                 ->where('results.tour_id', '=', $item->tour_id)
+                                 ->where('results.place', '=', $item->place)
+                                 ->where('results.sportsman_id', '<>', $item->sportsman_id)
+                                 ->select('sportsmen.sportsman')
+                                 ->value('sportsman')
+                                 }}
+                            </td>
+                            <td> {{ $item->haul }}</td>
+                            <td> {{ $s = \Illuminate\Support\Facades\DB::table('sportsmen')
+                 ->join('results', 'sportsmen.id', '=', 'results.sportsman_id')
+                 ->where('results.contest_id', '=', $item->contest_id)
+                 ->where('results.tour_id', '=', $item->tour_id)
+                 ->where('results.place', '=', $item->place)
+                 ->where('results.sportsman_id', '<>', $item->sportsman_id)
+                 ->select('results.haul')
+                 ->value('haul')
+                 }}</td>
+                            <td>{{ $item->point }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
             </div>
-            <a class="mb-1">
-                <a href="/cards/contest/{{ $item->contest_id }}/sportsman/{{ $item->id }}"><button class="btn btn-outline-success">Перейти в карточку</button></a>
-                <a href="/contest/{{ $item->contest_id }}/sportsman/edit/{{ $item->id }}"><button class="btn btn-outline-primary">Редактировать спортсмена</button></a>
-            </p>
-            <div class="text-dark"></div>
-        </div>
+            <div class="form-group">
+                <a href="/cards/edit/contest/{{ $item->contest_id }}/sportsman/{{ $item->sportsman_id }}"><button class="btn btn-outline-success">Редактировать карточку</button></a>
+                <a href="/contest/{{ $item->contest_id }}/sportsman/edit/{{ $item->sportsman_id }}"><button class="btn btn-outline-primary">Редактировать спортсмена</button></a>
+            </div>
+            <br>
         @endforeach
-    </div>
+        <div class="back">
+            <a href="{{ route('listContest') }}"><button class="btn btn-outline-primary">К списку сорвенований</button></a>
+        </div>
 @endsection
