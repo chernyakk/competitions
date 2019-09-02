fa211682922e1dae4d852febd28c72e15f2f4d07
Alexander Kirshin <alexander.kirshin.dev@gmail.com>
2019-09-02 16:51:17 +0000

    change project name Trout Anglers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ route('home')}}">
-                    {{ env('BRAND') }}
+                    Trout Anglers
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
