68ba54b1712bb73dd22ed7d67e6e9896f53b305d
Alexander Kirshin <alexander.kirshin.dev@gmail.com>
2019-09-06 11:15:29 +0000

    disable auth in header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,10 +46,10 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
+                            <!--
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('auth.Login') }}</a>
                             </li>
-                            <!--
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('auth.Register') }}</a>
